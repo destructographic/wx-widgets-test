04ee4e9f5bbe9dc7ea20723d1061d6156221b6cd
justin@archbox <driftpattern@gmail.com>
2023-07-25 23:03:29 -0700

    set content area to 50x100 px

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 class MyFrame : public wxFrame {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
+        : wxFrame(NULL, wxID_ANY, title, pos, wxSize(200, 100), wxDEFAULT_FRAME_STYLE & ~wxRESIZE_BORDER) // Set initial size to 200x100 pixels
     {
         // Create a panel to hold other controls (widgets)
         wxPanel* panel = new wxPanel(this, wxID_ANY);
@@ -11,8 +11,8 @@ public:
         // Create a label (text) control
         wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Hello, wxWidgets!", wxPoint(10, 10));
 
-        // Set the frame's size to fit its contents
-        Fit();
+        // Set the frame's content size to 50x100 pixels
+        SetClientSize(50, 100);
     }
 };
 
@@ -20,7 +20,7 @@ class MyApp : public wxApp {
 public:
     virtual bool OnInit() {
         // Create and show the main window
-        MyFrame* frame = new MyFrame("wxWidgets Window", wxPoint(50, 50), wxSize(400, 200));
+        MyFrame* frame = new MyFrame("", wxPoint(50, 50), wxSize(50, 100));
         frame->Show(true);
         return true;
     }
